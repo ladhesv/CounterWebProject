b78e3116c0fb38a654e70233bd1a773518c0bc27
ladhesv <34367800+ladhesv@users.noreply.github.com>
2017-12-09 11:57:48 +0530

    Update calcTest.java

--- test/main/java/com/qaagility/controller/calcTest.java
+++ test/main/java/com/qaagility/controller/calcTest.java
@@ -1,3 +1,25 @@
 package com.qaagility.controller;
+
+import org.uint.Test;
+import static org.junit.Assert.assertEquals;
+
 public class calcTest{
+  public void testSlow(){
+    Syste.out.println("Slow");
+  }
+  
+  public void testSlower(){
+    Syste.out.println("Slower");
+  }
+  
+  public void testFast(){
+    Syste.out.println("Fast");
+  }
+  
+  @Test
+  public void testCalc(){
+    assertEquals("Result",9,9);
+    //Syste.out.println("TestCalc =" +9*9);
+  }
+  
 }
