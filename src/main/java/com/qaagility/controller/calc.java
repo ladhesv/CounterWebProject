84d54bec228aa1035c9b8bf9f0e15d0cf26dee24
ladhesv <34367800+ladhesv@users.noreply.github.com>
2017-12-09 11:38:31 +0530

    Update calc.java

--- src/main/java/com/qaagility/controller/calc.java
+++ src/main/java/com/qaagility/controller/calc.java
@@ -1,7 +1,8 @@
 package com.qaagility.controller;
 public class calc{
 public static void main(String args[]){
-System.out.println("Multiplication 3*6 =" new calc().mul());
+  calc cl= new calc();
+System.out.println("Multiplication 3*6 =" cl.mul());
 }
 private int mul(){
 return 3*6;
