b7135f776dc4d4cf91730e2f6cef2c861abe52f1
ladhesv <34367800+ladhesv@users.noreply.github.com>
2017-12-09 11:29:04 +0530

    Create calc.java

---
+++ src/main/java/com/qaagility/controller/calc.java
@@ -0,0 +1,9 @@
+package com.qaagility.controller;
+public class calc{
+public static void main(String args[]){
+System.out.println("Multiplication 3*6 =" new calc().mul());
+}
+private int mul(){
+return 3*6;
+}
+}
